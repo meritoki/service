fbcf2eae3639da6ecaff0b35f7ce0371f7ad59c7
Joaquin Osvaldo Rodriguez <meritoki@yahoo.com>
2018-12-30 20:12:31 -0300

    Stable adding other interfaces

--- controller/interface/service/path.js
+++ controller/interface/service/path.js
@@ -12,19 +12,19 @@ exports.delete = function(router) {
 
 exports.get = function(router, passport) {
   console.log('service.path.get()');
-  router.get('/api/dialog/authorize', oauth2.authorization);
-  router.get("/api/login", service.getAPILogin);
-  router.get("/api/login/failure", service.getAPILoginFailure);
-  router.get("/api/logout", service.getLogout);
+  router.get('/v1/service/dialog/authorize', oauth2.authorization);
+  router.get("/v1/service/login", service.getAPILogin);
+  router.get("/v1/service/login/failure", service.getAPILoginFailure);
+  router.get("/v1/service/logout", service.getLogout);
 
   router.get("/v1/msg/email/verify", msg.getVerify);
 };
 
 exports.post = function(router, passport) {
   console.log('service.path.post()');
-  router.post('/api/dialog/authorize/decision', oauth2.decision);
-  router.post('/api/oauth/token', oauth2.token);
-  router.post("/api/login", service.postAPILogin);
+  router.post('/v1/service/dialog/authorize/decision', oauth2.decision);
+  router.post('/v1/service/oauth/token', oauth2.token);
+  router.post("/v1/service/login", service.postAPILogin);
 
 //auth
   router.post("/v1/auth/name/password",connectEnsureLogin.ensureLoggedIn(), auth.postNamePasswordAuth);
@@ -34,6 +34,9 @@ exports.post = function(router, passport) {
   router.post("/v1/user/id",connectEnsureLogin.ensureLoggedIn(),  user.postIDUser);
 //msg
   router.post("/v1/msg/email/verification",msg.postEmailVerification);
+//id
+
+//location
 
   // connectEnsureLogin.ensureLoggedIn(),
 };
